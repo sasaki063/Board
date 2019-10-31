7bf519254232aaae166c999c2618c51ce07f1efc
hirotomo.sasaki <sasakiccc063@gmail.com>
2019-10-31 17:49:13 +0900

    migration file修正 user_id

--- db/migrate/20191031063151_create_posts.rb
+++ db/migrate/20191031063151_create_posts.rb
@@ -3,8 +3,10 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.string :name
       t.string :category
+      t.references :user, foregin_key: true
 
       t.timestamps
     end
+    add_index :posts, [:user_id, :created_at]
   end
 end
