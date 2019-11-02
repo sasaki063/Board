8e9ce97028a5fbe3ab5561b1607188a640c08df1
hirotomo.sasaki <sasakiccc063@gmail.com>
2019-11-02 10:08:12 +0900

    model修正 comment_optional: ture追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id
-    if @comment.save
+    if @comment.save!
       redirect_to posts_path
     else
       redirect_to posts_path
